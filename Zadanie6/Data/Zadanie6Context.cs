4d1f1dfcf969b1887e43229ab8f3824f6f49a863
Mariusz <Mariusz@LAPTOP-92MQ158U>
2023-09-15 20:12:59 +0200

    Dodaj pliki projektów.

---
+++ Zadanie6/Data/Zadanie6Context.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Zadanie6;
+using Zadanie6.Models;
+
+namespace Zadanie6.Data
+{
+    public partial class Zadanie6Context : DbContext
+    {
+        public Zadanie6Context()
+        {
+        }
+
+        public Zadanie6Context(DbContextOptions<Zadanie6Context> options)
+            : base(options)
+        {
+        }
+        public DbSet<Purchase> Purchases { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UsePropertyAccessMode(PropertyAccessMode.Property);
+        }
+    }
+}
+
---
+++ Zadanie6/ViewModels/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Zadanie6.Data;
+using Zadanie6.Models;
+
+namespace Zadanie6.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    private readonly Zadanie6Context context;
+
+    public MainViewModel(Zadanie6Context context)
+    {
+        this.context = context;
+        Purchases = context.Purchases
+            .LoadAsync()
+            .ContinueWith(p => context.Purchases.Local.ToObservableCollection());
+    }
+    [RelayCommand]
+    private async Task AddPurchaseAsync()
+    {
+        if (NewPurchase.Validate())
+        {
+            context.Add(NewPurchase);
+            await context.SaveChangesAsync();
+            NewPurchase = new Purchase();
+        }
+    }
+    [RelayCommand]
+    private async Task DeletePurchaseAsync(Purchase purchase)
+    {
+        context.Remove(purchase);
+        await context.SaveChangesAsync();
+    }
+
+  
+
+    [ObservableProperty]
+    private Purchase newPurchase = new Purchase();
+
+    private TaskNotifier<ObservableCollection<Purchase>> purchases;
+    public Task<ObservableCollection<Purchase>> Purchases
+    {
+        get => purchases;
+        set => SetPropertyAndNotifyOnCompletion(ref purchases, value);
+    }
+
+}
